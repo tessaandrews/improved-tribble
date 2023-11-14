1756b7da5aade3ec7a4aba8be67914ffdc0fcdd3
tessaandrews <142922842+tessaandrews@users.noreply.github.com>
2023-11-13 22:15:17 -0600

    updated login and signup routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
-
-
+const userRoutes = require('./userRoutes');
 const userRoutes = require('./userRoutes');
 
-
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,112 +1,83 @@
-const router = require("express").Router();
-const { Post, User,Comment } = require("../models");
-// Import the custom middleware
-//const withAuth = require("../utils/auth");
+const router = require('express').Router();
+const { Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
-// GET all posts for the homepage
-router.get("/", async (req, res) => {
+router.get('/', async (req, res) => {
   try {
+    // Get all projects and JOIN with user data
     const PostData = await Post.findAll({
       include: [
         {
           model: User,
-          attributes: {exclude:'passwords'},
-          
+          attributes: {exclude: 'passwords'},
         },
       ],
     });
 
-    const posts = PostData.map((post) =>
-      post.get({ plain: true })
-    );
-      console.log(posts)
-    
-    res.render("homepage", {
-      posts,
-      loggedIn: req.session.loggedIn,
+    // Serialize data so the template can read it
+    const posts = PostData.map((post) => post.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      posts, 
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
-// GET one blogpost
-router.get("/post/:id", async (req, res) => {
+router.get('/post/:id', async (req, res) => {
   try {
     const PostData = await Post.findByPk(req.params.id, {
       include: [
-        {model:User, attributes:{exclude:'password'}},
+        { model: User, attributes: {exclude: 'password'}},
         {
-          model: Comment, include:[{model:User, attributes:{exclude:'password'}}]
-         
+          model: Comment, inclide: [{model: User, attributes: {exclude:'password'}}]
+
         },
       ],
     });
 
     const posts = PostData.get({ plain: true });
-    console.log("Checking Posts")
-    console.log(posts)
-  
-    res.render("singlePost", { posts, loggedIn: req.session.loggedIn });
+
+    res.render('post', {
+      ...posts,
+      logged_in: req.session.logged_in
+    });
   } catch (err) {
-    console.log(err);
     res.status(500).json(err);
   }
 });
 
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
 
+    const user = userData.get({ plain: true });
 
-// Create a route to handle the form submission
-// router.post('/create-post', withAuth, async (req, res) => {
-//   try {
-//     // Get the data from the form
-//     const { title, category, description } = req.body;
-
-//     // Create a new blog post in the database
-//     const newPost = await Post.create({
-//       title,
-//       description,
-//       category,
-//       // Include other properties as needed
-//     });
-
-//     // Redirect to the page that displays the newly created blog post
-//     res.redirect(`/Post/${newPost.id}`);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-// Create a route to handle the form submission
-// router.post('/create-comment', withAuth, async (req, res) => {
-//   try {
-//     // Get the data from the form
-//     const { description } = req.body;
-
-//     const Comments = dbCommentData.get({ plain: true });
-
-//     // Create a new comment in the database
-//     const newComment = await Comments.create({
-//       description,
-//       // Include other properties as needed
-//     });
-
-//     // Redirect to the page that displays the newly created blog post
-//     res.redirect(`/Post/`);
-//   } catch (err) {
-//     console.error(err);
-//     res.status(500).json(err);
-//   }
-// });
-
-router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
-    res.redirect("/");
-  } else {
-    res.render("login");
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
   }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
